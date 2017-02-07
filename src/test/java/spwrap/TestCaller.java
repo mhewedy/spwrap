71d57491a455516896c6d03bd88b6dc8847b4a36
Muhammad Hewedy <mhewedy@elm.sa>
2017-02-07 15:40:17 +0300

    releasing

--- src/test/java/spwrap/TestCaller.java
+++ src/test/java/spwrap/TestCaller.java
@@ -40,7 +40,7 @@ public class TestCaller {
 	@Before
 	public void setup() {
 		HikariDataSource ds = new HikariDataSource();
-		ds.setJdbcUrl("jdbc:sqlserver://localhost:1433;DatabaseName=TEST");
+		ds.setJdbcUrl("jdbc:sqlserver://192.168.43.12:1433;DatabaseName=TEST");
 		ds.setUsername("test");
 		ds.setPassword("test");
 
