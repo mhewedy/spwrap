3ff68c015dd4f9eea4b8f3f6a61f818435ffb483
Muhammad Hewedy <mhewedy@gmail.com>
2017-02-21 22:46:36 +0300

    refactor test class

--- src/test/groovy/spwrap/DAOTest.groovy
+++ src/test/groovy/spwrap/DAOTest.groovy
@@ -25,10 +25,8 @@ class DAOTest extends Specification{
 		given:
 			def firstName = "Abdullah"
 			def lastName = "Mohammad"
-		
 		when:
 			customerDao.createCustomer0(firstName, lastName)
-			
 		then:
 			noExceptionThrown()
 	}
@@ -37,7 +35,6 @@ class DAOTest extends Specification{
 		given:
 			def firstName = "Abdullah"
 			def lastName = "Mohammad"
-		
 		when:
 			def custId = customerDao.createCustomer(firstName, lastName)		
 		then:
@@ -48,7 +45,6 @@ class DAOTest extends Specification{
 		given:
 			def firstName = "Abdullah"
 			def lastName = "Mohammad"
-		
 		when:
 			def custId0 = customerDao.createCustomer(firstName, lastName)		
 			def custId1 = customerDao.createCustomer(firstName, lastName)		
@@ -65,7 +61,6 @@ class DAOTest extends Specification{
 		given:
 			def firstName = "Abdullah"
 			def lastName = "Mohammad"
-		
 		when:
 			customerDao.createCustomer0(firstName, lastName)
 			def customer = customerDao.getCustomer2(0);
@@ -80,7 +75,6 @@ class DAOTest extends Specification{
 		given:
 			def firstName = "Abdullah"
 			def lastName = "Mohammad"
-		
 		when:
 			customerDao.createCustomer0(firstName, lastName)
 			def customer = customerDao.getCustomer1(0);
@@ -95,11 +89,9 @@ class DAOTest extends Specification{
 		given:
 			def firstName = "Farida"
 			def lastName = "Mohammad"
-			
 		when:
 			customerDao.createCustomer0(firstName, lastName)
 			def customer = customerDao.getCustomer3(0);
-		
 		then:
 			customer.firstName() == firstName
 			customer.lastName() == lastName
@@ -110,11 +102,9 @@ class DAOTest extends Specification{
 		given:
 			def firstName = "Farida"
 			def lastName = "Mohammad"
-			
 		when:
 			customerDao.createCustomer0(firstName, lastName)
-			def customer = customerDao.getCustomer4(0);
-		
+			customerDao.getCustomer4(0);
 		then:
 			thrown(CallException)
 	}
@@ -123,11 +113,9 @@ class DAOTest extends Specification{
 		given:
 			def firstName = "Farida"
 			def lastName = "Mohammad"
-			
 		when:
 			customerDao.createCustomer0(firstName, lastName)
-			def customer = customerDao.getCustomer5(0);
-		
+			customerDao.getCustomer5(0);
 		then:
 			thrown(CallException)
 	}
@@ -137,28 +125,23 @@ class DAOTest extends Specification{
 		given:
 			def firstName = "Farida"
 			def lastName = "Mohammad"
-			
 		when:
 			customerDao.createCustomer0(firstName, lastName)
-			def customer = customerDao.getCustomer6(0);
-		
+			customerDao.getCustomer6(0);
 		then:
 			thrown(CallException)
 	}
 	
 	def "using implicit ResultSet Mapper to map result set"(){
-	
 		given:
 			def firstName1 = "Farida"
 			def lastName1 = "Mohammad"
 			def firstName2 = "Abdullah"
 			def lastName2 = "Mohammad"
-			
 		when:
 			customerDao.createCustomer0(firstName1, lastName1)
 			customerDao.createCustomer0(firstName2, lastName2)
 			def list = customerDao.listCustomers2();
-			
 		then:
 			2 == list.size()
 			with(list.get(0)){
@@ -173,18 +156,15 @@ class DAOTest extends Specification{
 	}
 	
 	def "using explicit ResultSet Mapper to map result set"(){
-	
 		given:
 			def firstName1 = "Farida"
 			def lastName1 = "Mohammad"
 			def firstName2 = "Abdullah"
 			def lastName2 = "Mohammad"
-			
 		when:
 			customerDao.createCustomer0(firstName1, lastName1)
 			customerDao.createCustomer0(firstName2, lastName2)
 			def list = customerDao.listCustomers1();
-			
 		then:
 			2 == list.size()
 			with(list.get(0)){
@@ -200,18 +180,15 @@ class DAOTest extends Specification{
 	
 	
 	def "returning a Tuple as a result of stored proc return both result set and output parameters" (){
-		
 		given:
 			def firstName1 = "Farida"
 			def lastName1 = "Mohammad"
 			def firstName2 = "Abdullah"
 			def lastName2 = "Mohammad"
-			
 		when:
 			customerDao.createCustomer0(firstName1, lastName1)
 			customerDao.createCustomer0(firstName2, lastName2)
 			def tuple = customerDao.listCustomersWithDate();
-			
 		then:
 			tuple.object != null
 			
@@ -228,18 +205,15 @@ class DAOTest extends Specification{
 	}
 	
 	def "StoredProc annotation will use method name as stored procedure name if no one is specified" (){
-	
 		given:
 			def firstName1 = "Farida"
 			def lastName1 = "Mohammad"
 			def firstName2 = "Abdullah"
 			def lastName2 = "Mohammad"
-			
 		when:
 			customerDao.createCustomer0(firstName1, lastName1)
 			customerDao.createCustomer0(firstName2, lastName2)
 			def tuple = customerDao.list_customers_with_date();
-			
 		then:
 			tuple.object != null
 			
@@ -256,23 +230,20 @@ class DAOTest extends Specification{
 	}
 	
 	def "stored procedure will return error code from database" (){
-		
 		when:
 			customerDao.callStoredProcWithError();
 		then:
 			def e = thrown(CallException)
 			e.message.startsWith("Stored Procedure returns error code 1")
-			e.errorCode == 1
+			e.errorCode == (short)1
 	}
 	
 	def "choose to disable the feature of result code and message, only we care about business related parameters" (){
 		given:
 			def dao = new DAO.Builder(TestUtils.ds).config(new Config().useStatusFields(false)).build()
 			def customerDao2 = dao.create(CustomerDAO.class)
-			
 		when:
 			customerDao2.getFirstTableNameNoResultFields()
-
 		then:
 			noExceptionThrown()
 		
@@ -282,10 +253,8 @@ class DAOTest extends Specification{
 		given:
 			def dao = new DAO.Builder(TestUtils.ds).config(new Config().successCode((short) 1)).build()
 			def customerDao2 = dao.create(CustomerDAO.class);
-		
 		when:
 			customerDao2.callStoredProcWithError()
-			
 		then:
 			noExceptionThrown()
 	}
@@ -293,7 +262,6 @@ class DAOTest extends Specification{
 	def "yet another test case for result set mapper" (){
 		when:
 			def listTables = customerDao.listTables();
-			
 		then:
 			listTables.size() > 2
 			listTables.contains("ROUTINES")
@@ -302,8 +270,7 @@ class DAOTest extends Specification{
 	
 	def "you can use @Scalar with Output parameters only mapper" (){
 		when:
-			def listTables = customerDao.listTables2();
-			
+			customerDao.listTables2();
 		then:
 			thrown(CallException)
 	}
@@ -311,7 +278,6 @@ class DAOTest extends Specification{
 	def "when stored procedure have missing @Param annotations on the parameters, IllegalArgumentException will be thrown" (){
 		when:
 			customerDao.createCustomer7("Farida", "Muhammad");
-		
 		then:
 			thrown(IllegalArgumentException)
 	}
@@ -320,17 +286,14 @@ class DAOTest extends Specification{
 	@Ignore
 	@ConfineMetaClassChanges([CallableStatement])
 	def "Result of output parameter getInt throws SQLException" (){
-	
 		given:
 			def sqlExceptionMsg = "exception happend while tring to call getInt"
 			CallableStatement.metaClass.getObject = { int parameterIndex -> throw new SQLException(sqlExceptionMsg)}
-	
 		when:
-			def custId = customerDao.createCustomer("Abdullah", "Mohammad")
-			
+			customerDao.createCustomer("Abdullah", "Mohammad")
 		then:
 			def e = thrown(CallException)
-			e.cause == SQLException
+			e.cause.class == SQLException
 			e.cause.message == sqlExceptionMsg
 	}
 
