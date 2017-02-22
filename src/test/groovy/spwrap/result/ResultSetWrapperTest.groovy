431f1a19f43154bb4deb484709c8d9a81f73eae6
Muhammad Hewedy <mhewedy@elm.sa>
2017-02-22 14:17:28 +0300

    add more test cases for ResultSetWrapper

--- src/test/groovy/spwrap/result/ResultSetWrapperTest.groovy
+++ src/test/groovy/spwrap/result/ResultSetWrapperTest.groovy
@@ -19,37 +19,133 @@ class ResultSetWrapperTest extends Specification {
 
     def "resultSet getString(int)"() {
         given:
-            def someColumnIndex = 1
+        def someColumnIndex = 1
         when:
-            resultSetWrapper.getString(someColumnIndex)
+        resultSetWrapper.getString(someColumnIndex)
         then:
-            1 * resultSetMock.getString(someColumnIndex)
+        1 * resultSetMock.getString(someColumnIndex)
     }
 
-    def "resultSet getString(any integer value)"() {
+    def "resultSet getString(columnIndex)"() {
         when:
-            resultSetWrapper.getString(1)
+        resultSetWrapper.getString(1)
         then:
-            1 * resultSetMock.getString(_ as Integer)
+        1 * resultSetMock.getString(_ as Integer)
     }
 
-    def "resultSet getDouble(any integer value)"() {
-        when:
-            resultSetWrapper.getDouble(1)
-        then:
-            1 * resultSetMock.getDouble(_ as Integer)
-    }
-
-
-    def "resultSet getXXX(XXX) throws SQLException, then resultSetWapper.getXXX(XXX) should fails"(){
+    def "resultSet getInteger(int)"() {
         given:
-            def exceptionMsg = "I thrown an exception"
+        def someColumnIndex = 1
         when:
-            resultSetMock./get.*/(_) >> {throw new SQLException(exceptionMsg)}
-            resultSetWrapper.getString(1)
+        resultSetWrapper.getInteger(someColumnIndex)
         then:
-            def e = thrown(CallException)
-            e.cause.class == SQLException
-            e.cause.message == exceptionMsg
+        1 * resultSetMock.getInteger(someColumnIndex)
+    }
+
+    def "resultSet getDouble(int)"() {
+        given:
+        def someColumnIndex = 1
+        when:
+        resultSetWrapper.getDouble(someColumnIndex)
+        then:
+        1 * resultSetMock.getDouble(someColumnIndex)
+    }
+
+    def "resultSet getLong(int)"() {
+        given:
+        def someColumnIndex = 1
+        when:
+        resultSetWrapper.getLong(someColumnIndex)
+        then:
+        1 * resultSetMock.getLong(someColumnIndex)
+    }
+
+    def "resultSet getNString(int)"() {
+        given:
+        def someColumnIndex = 1
+        when:
+        resultSetWrapper.getNString(someColumnIndex)
+        then:
+        1 * resultSetMock.getNString(someColumnIndex)
+    }
+
+    def "resultSet getShort(int)"() {
+        given:
+        def someColumnIndex = 1
+        when:
+        resultSetWrapper.getShort(someColumnIndex)
+        then:
+        1 * resultSetMock.getShort(someColumnIndex)
+    }
+
+
+    def "resultSet getNClob(int)"() {
+        given:
+        def someColumnIndex = 1
+        when:
+        resultSetWrapper.getNClob(someColumnIndex)
+        then:
+        1 * resultSetMock.getNClob(someColumnIndex)
+    }
+
+
+    def "resultSet getSQLXML(int)"() {
+        given:
+        def someColumnIndex = 1
+        when:
+        resultSetWrapper.getSQLXML(someColumnIndex)
+        then:
+        1 * resultSetMock.getSQLXML(someColumnIndex)
+    }
+
+
+    def "resultSet getBigDecimal(int)"() {
+        given:
+        def someColumnIndex = 1
+        when:
+        resultSetWrapper.getBigDecimal(someColumnIndex)
+        then:
+        1 * resultSetMock.getBigDecimal(someColumnIndex)
+    }
+
+
+    def "resultSet getCharacterStream(int)"() {
+        given:
+        def someColumnIndex = 1
+        when:
+        resultSetWrapper.getCharacterStream(someColumnIndex)
+        then:
+        1 * resultSetMock.getCharacterStream(someColumnIndex)
+    }
+
+
+    def "resultSet getTimestamp(int)"() {
+        given:
+        def someColumnIndex = 1
+        when:
+        resultSetWrapper.getTimestamp(someColumnIndex)
+        then:
+        1 * resultSetMock.getTimestamp(someColumnIndex)
+    }
+
+    def "resultSet getgetBytes(int)"() {
+        given:
+        def someColumnIndex = 1
+        when:
+        resultSetWrapper.getgetBytes(someColumnIndex)
+        then:
+        1 * resultSetMock.getgetBytes(someColumnIndex)
+    }
+
+    def "resultSet getXXX(XXX) throws SQLException, then resultSetWapper.getXXX(XXX) should fails"() {
+        given:
+        def exceptionMsg = "I thrown an exception"
+        when:
+        resultSetMock./get.*/(_) >> { throw new SQLException(exceptionMsg) }
+        resultSetWrapper.getString(1)
+        then:
+        def e = thrown(CallException)
+        e.cause.class == SQLException
+        e.cause.message == exceptionMsg
     }
 }
