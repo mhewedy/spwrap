15a2b8d44889ab8d53e82fbced5a9fbbc8e43442
Muhammad Hewedy <mhewedy@gmail.com>
2017-03-03 14:57:39 +0300

    add javadoc

--- src/main/java/spwrap/DAO.java
+++ src/main/java/spwrap/DAO.java
@@ -6,6 +6,21 @@ import javax.sql.DataSource;
 
 import spwrap.proxy.DAOInvocationHandler;
 
+/**
+ * <p>
+ * Used to create new instance of DAO classes
+ * <pre>
+ * {@code
+ *Config optionalConfig = //
+ *   DAO dao = new DAO.Builder(dataSource).config(optionalConfig).build();
+ *   MyDAO myDao = dao.create(MyDAO.class);
+ * }
+ * </pre>
+ * DAO objects created by {@link #create(Class)} method are thread-safe and can be cached.
+ * <p>
+ * For example, in spring framework you can create spring @Bean for each of your DAOs.
+ * <p>
+ */
 public class DAO {
 
     private final Caller caller;
