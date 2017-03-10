83501fa3a8b2ebfd382205d144d5d73c3391065c
Muhammad Hewedy <mhewedy@gmail.com>
2017-03-11 02:56:49 +0300

    fixing travis-ci

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -6,4 +6,5 @@ else
     echo "not on a tag -> keep snapshot version in pom.xml"
 fi
 
-mvn clean deploy --settings .travis/settings.xml -DskipTests=true -B -U
+#mvn clean deploy --settings .travis/settings.xml -DskipTests=true -B -U
+mvn clean deploy --settings .travis/settings.xml -B -U
